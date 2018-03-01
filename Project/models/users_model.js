099eb234ea741b92c10fcd9116ee0ba9438ccd43
dnnxl15 <dnnxl15@hotmail.com>
2018-02-28 22:37:56 -0600

    MEAN Stack

---
+++ Project/models/users_model.js
@@ -0,0 +1,9 @@
+﻿ var mongoose = require('mongoose'),
+                         Schema = mongoose.Schema;
+ var UserSchema = new Schema({
+                     username: {type: String, unique: true},
+                     email: String,
+                     color: String,
+                     hashed_password: String
+   });
+mongoose.model('User', UserSchema);
